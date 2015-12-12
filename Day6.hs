efe13637e0b6ea677acbeb7d7fb3e87958939f16
Tom Spurling <tom@tspurling.co.uk>
2015-12-12 01:55:16 +0000

    part 2 of day6

--- Day6.hs
+++ Day6.hs
@@ -1,13 +1,16 @@
 module Day6 where
 
-initialState n = replicate n (replicate n False)
+initialState n = replicate n (replicate n 0)
 
+type Brightness = Int
 data Fs = Never | Always | Flip
   deriving Show
 
-f Never = \x->False
-f Always = \x->True
-f Flip = not
+f Never x
+  | x <= 0 = 0
+  | otherwise = x-1
+f Always x = (x+1)
+f Flip x = (x+2)
 
 applyRange f (x1,x2) aa = safeX1 ++ map f inX ++ safeX2
   where
@@ -16,7 +19,7 @@ applyRange f (x1,x2) aa = safeX1 ++ map f inX ++ safeX2
 
 applySq f (x1,y1) (x2,y2) aa = applyRange (applyRange f (y1,y2)) (x1,x2) aa
 
-applyLine :: String -> [[Bool]] -> [[Bool]]
+applyLine :: String -> [[Brightness]] -> [[Brightness]]
 applyLine l = applySq (f fs) (x1,y1) (x2,y2)
   where
     (fs, (x1,y1):(x2,y2):[]) = interpretLine l
@@ -29,12 +32,12 @@ interpretLine l = i $ words l
     i ("toggle":rest)     = (Flip, c rest)
     c (a:"through":b:_)   = map (\(a,b)->(read a, (read.tail) b)) $ map (break (==',')) [a,b]
 
-followInstructions :: [String] -> [[Bool]] -> [[[Bool]]]
+followInstructions :: [String] -> [[Brightness]] -> [[[Brightness]]]
 followInstructions instrs init = init : zipWith (\f x-> f x) (map applyLine instrs) (followInstructions instrs init)
 
-conclude :: [String] -> [[Bool]]
+conclude :: [String] -> [[Brightness]]
 conclude instrs = last $ followInstructions instrs (initialState 1000)
 
 main = do
   input <- getContents
-  putStrLn.show $ sum $ map (length . filter id) $ conclude $ lines input
+  putStrLn.show $ sum $ map sum $ conclude $ lines input
